8db181af81707a0449aef5b7ddc865e628ecf049
leandro8080 <leandro.schultze@ict.csbe.ch>
2024-11-27 15:48:56 +0100

    Changed schema
    Changed the schema to match the ERM
    
    Fixed bug with command "start".

---
+++ server/database/database.ts
@@ -0,0 +1,38 @@
+import mariadb from "mariadb";
+import { Pool } from "mariadb";
+import { users, posts, comments, likes } from "./schema";
+
+export class Database {
+    // Properties
+    private _pool: Pool;
+    // Constructor
+    constructor() {
+        this._pool = mariadb.createPool({
+            database: process.env.DB_NAME || "minitwitter",
+            host: process.env.DB_HOST || "localhost",
+            user: process.env.DB_USER || "minitwitter",
+            password: process.env.DB_PASSWORD || "supersecret123",
+            connectionLimit: 5,
+        });
+        this.initializeDBSchema();
+    }
+    // Methods
+    private initializeDBSchema = async () => {
+        console.log("Initializing DB schema...");
+        await this.executeSQL(users);
+        await this.executeSQL(posts);
+        await this.executeSQL(comments);
+        await this.executeSQL(likes);
+    };
+
+    public executeSQL = async (query: string) => {
+        try {
+            const conn = await this._pool.getConnection();
+            const res = await conn.query(query);
+            conn.end();
+            return res;
+        } catch (err) {
+            console.log(err);
+        }
+    };
+}
---
+++ server/database/schema.ts
@@ -0,0 +1,43 @@
+const users = `
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username text NOT NULL UNIQUE,
+    password text NOT NULL,
+    role VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+`;
+
+const posts = `
+CREATE TABLE IF NOT EXISTS posts (
+    id INT NOT NULL AUTO_INCREMENT,
+    userId INT NOT NULL,
+    content text NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userId) REFERENCES users(id)
+);
+`;
+
+const comments = `
+CREATE TABLE IF NOT EXISTS comments (
+    id INT NOT NULL AUTO_INCREMENT,
+    userId INT NOT NULL,
+    content text NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userId) REFERENCES users(id)
+);
+`;
+
+const likes = `
+CREATE TABLE IF NOT EXISTS likes (
+    id INT NOT NULL AUTO_INCREMENT,
+    postId INT NOT NULL,
+    userId INT NOT NULL,
+    isPositive boolean NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (postId) REFERENCES posts(id),
+    FOREIGN KEY (userId) REFERENCES users(id)
+);
+`;
+
+export { users, posts, comments, likes };
